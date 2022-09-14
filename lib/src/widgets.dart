86705c2ecc8a8471f40cb489a4c136299b07e65f
tsaniarisqi <tsaniarisqi98@gmail.com>
2022-09-14 21:27:13 +0700

    dapatkan kode sample

---
+++ lib/src/widgets.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  final String heading;
+
+  const Header(String s, {Key? key, required this.heading}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          heading,
+          style: const TextStyle(fontSize: 24),
+        ),
+      );
+}
+
+class Paragraph extends StatelessWidget {
+  final String content;
+
+  const Paragraph(String s, {Key? key, required this.content})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: Text(
+          content,
+          style: const TextStyle(fontSize: 18),
+        ),
+      );
+}
+
+class IconAndDetail extends StatelessWidget {
+  final IconData icon;
+  final String detail;
+
+  const IconAndDetail({Key? key, required this.icon, required this.detail}) : super(key: key);  
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Icon(icon),
+            const SizedBox(width: 8),
+            Text(
+              detail,
+              style: const TextStyle(fontSize: 18),
+            )
+          ],
+        ),
+      );
+}
+
+class StyledButton extends StatelessWidget {
+  final Widget child;
+  final void Function() onPressed;
+
+  const StyledButton({Key? key, required this.child, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => OutlinedButton(
+        style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: Colors.deepPurple)),
+        onPressed: onPressed,
+        child: child,
+      );
+}
